e3f7f490be728319b4009829313ecf99cdf6bc95
gautamjha2002 <98412293+gautamjha2002@users.noreply.github.com>
2022-04-24 11:54:59 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -4,4 +4,7 @@ public class Greeter {
     public String sayHello() {
         return "DevOps Project for AchiStar Technologies";
     }
+    public String sayHii() {
+        return "DevOps Project for Learning Jenkins";
+    }
 }
